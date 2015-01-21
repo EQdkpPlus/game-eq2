0614461924011aa8bea970ed821d35a01a715de2
God Mod <godmod23@web.de>
2015-01-21 16:53:37 +0100

    Fixed Tooltip Settings

--- infotooltip/zam.class.php
+++ infotooltip/zam.class.php
@@ -37,25 +37,16 @@ if(!class_exists('zam')) {
 				'eq2' => array('icon_loc' => 'http://eq2.allakhazam.com/images/Icons/', 'icon_ext' => '.jpg', 'default_icon' => 'unknown'),
 			);
 			$this->settings = array(
-				'itt_icon_loc' => array(	'name' => 'itt_icon_loc',
-											'language' => 'pk_itt_icon_loc',
-											'fieldtype' => 'text',
-											'size' => false,
-											'options' => false,
+				'itt_icon_loc' => array(
+											'type' => 'text',
 											'default' => ((isset($g_settings[$this->config['game']]['icon_loc'])) ? $g_settings[$this->config['game']]['icon_loc'] : ''),
 				),
-				'itt_icon_ext' => array(	'name' => 'itt_icon_ext',
-											'language' => 'pk_itt_icon_ext',
-											'fieldtype' => 'text',
-											'size' => false,
-											'options' => false,
+				'itt_icon_ext' => array(
+											'type' => 'text',
 											'default' => ((isset($g_settings[$this->config['game']]['icon_ext'])) ? $g_settings[$this->config['game']]['icon_ext'] : ''),
 				),
-				'itt_default_icon' => array('name' => 'itt_default_icon',
-											'language' => 'pk_itt_default_icon',
-											'fieldtype' => 'text',
-											'size' => false,
-											'options' => false,
+				'itt_default_icon' => array(
+											'type' => 'text',
 											'default' => ((isset($g_settings[$this->config['game']]['default_icon'])) ? $g_settings[$this->config['game']]['default_icon'] : ''),
 				),
 			);
